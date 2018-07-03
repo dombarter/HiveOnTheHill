6263b4a1499f089d7fcb229a79be4445665532ca
Dom Barter <dominicbarter@gmail.com>
2018-07-03 10:06:22 +0100

    Added base uno code

---
+++ TheHiveUno.ino
@@ -0,0 +1,58 @@
+#include <TheThingsNetwork.h>
+#include <Wire.h>
+#include <math.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614(); 
+
+const char *appEui = "70B3D57ED001042F";
+const char *appKey = "13215CA3CA18A4327A2B68471F7C6601";
+    
+#define loraSerial Serial1
+#define debugSerial Serial
+#define freqPlan TTN_FP_EU868
+TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
+    
+void setup() {
+  loraSerial.begin(57600);
+  debugSerial.begin(9600);
+    
+  while (!debugSerial && millis() < 10000);
+
+  debugSerial.println("Connecting To 'The Things Network'");
+  ttn.join(appEui, appKey); 
+
+  mlx.begin();
+}
+
+void loop() {
+
+  int objTemp = 00; //codes for ambient and object
+  int ambTemp = 01;
+  
+  float tempReading = mlx.readObjectTempC();
+  int integerPart = (int)tempReading;
+  int decimalPart = ((tempReading - (float)integerPart)*100)+1;
+
+  byte object[3];
+  object[0] = objTemp;
+  object[1] = integerPart;
+  object[2] = decimalPart;
+
+  ttn.sendBytes(object, sizeof(object)); //sending object temperature
+
+  delay(3000);
+
+  tempReading = mlx.readAmbientTempC();
+  integerPart = (int)tempReading;
+  decimalPart = ((tempReading - (float)integerPart)*100)+1;
+
+  byte ambient[3];
+  ambient[0] = ambTemp;
+  ambient[1] = integerPart;
+  ambient[2] = decimalPart;
+  
+  ttn.sendBytes(ambient, sizeof(ambient)); //sending ambient temperature 
+
+  delay(3000);
+}
